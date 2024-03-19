56302822341ac15211a6347619b69b9d86f69c87
rogerkreienbuehl <r.kreienbuehl@gmx.ch>
2024-03-19 07:51:24 +0100

    fix: tile display error fixed

--- mapcompose/src/desktopMain/kotlin/dev/kreienbuehl/mapcompose/ui/view/TileCanvas.desktop.kt
+++ mapcompose/src/desktopMain/kotlin/dev/kreienbuehl/mapcompose/ui/view/TileCanvas.desktop.kt
@@ -6,8 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.asSkiaBitmap
+import androidx.compose.ui.graphics.asSkiaColorFilter
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.withTransform
@@ -17,6 +16,8 @@ import dev.kreienbuehl.mapcompose.core.Tile
 import dev.kreienbuehl.mapcompose.core.VisibleTilesResolver
 import dev.kreienbuehl.mapcompose.ui.state.ZoomPanRotateState
 import org.jetbrains.skia.Image
+import org.jetbrains.skia.Paint
+import org.jetbrains.skia.Rect
 
 @Composable
 internal actual fun TileCanvas(
@@ -59,15 +60,19 @@ internal actual fun TileCanvas(
                 val tileScaled = (tileSize / scaleForLevel).toInt()
                 val l = tile.col * tileScaled
                 val t = tile.row * tileScaled
+                val r = l + tileScaled
+                val b = t + tileScaled
+                val dest = Rect(l.toFloat(), t.toFloat(), r.toFloat(), b.toFloat())
 
                 val colorFilter = colorFilterProvider?.getColorFilter(tile.row, tile.col, tile.zoom)
 
-                paint.alpha = tile.alpha
-                paint.colorFilter = colorFilter
+                paint.alpha = (tile.alpha * 255).toInt()
+                if (colorFilter != null) {
+                    paint.colorFilter = colorFilter.asSkiaColorFilter()
+                }
 
                 drawIntoCanvas {
-//                    it.nativeCanvas.drawImage(tile.bitmap.toBufferedImage().toImage(), l.toFloat(), t.toFloat())
-                    it.nativeCanvas.drawImage(Image.makeFromEncoded(tile.bitmap), l.toFloat(), t.toFloat())
+                    it.nativeCanvas.drawImageRect(Image.makeFromEncoded(tile.bitmap),dest, paint)
                 }
 
                 /* If a tile isn't fully opaque, increase its alpha state by the alpha tick */
